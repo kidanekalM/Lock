b1cde976d46a29f79a3f2d39ee6325164a30b116
Kidanekal <akidanekal@gmail.com>
2023-02-18 23:00:02 +0900

    Updated lock

---
+++ lock/Form1.Designer.cs
@@ -0,0 +1,111 @@
+﻿namespace @lock
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.components = new System.ComponentModel.Container();
+            this.txt_paswd = new System.Windows.Forms.TextBox();
+            this.txt_DataInOut = new System.Windows.Forms.RichTextBox();
+            this.timer1 = new System.Windows.Forms.Timer(this.components);
+            this.timer2 = new System.Windows.Forms.Timer(this.components);
+            this.SuspendLayout();
+            // 
+            // txt_paswd
+            // 
+            this.txt_paswd.AcceptsTab = true;
+            this.txt_paswd.Anchor = ((System.Windows.Forms.AnchorStyles)((((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Bottom) 
+            | System.Windows.Forms.AnchorStyles.Left) 
+            | System.Windows.Forms.AnchorStyles.Right)));
+            this.txt_paswd.BackColor = System.Drawing.SystemColors.ActiveCaptionText;
+            this.txt_paswd.BorderStyle = System.Windows.Forms.BorderStyle.None;
+            this.txt_paswd.ForeColor = System.Drawing.SystemColors.InactiveCaption;
+            this.txt_paswd.Location = new System.Drawing.Point(242, 12);
+            this.txt_paswd.Name = "txt_paswd";
+            this.txt_paswd.PasswordChar = ' ';
+            this.txt_paswd.Size = new System.Drawing.Size(263, 13);
+            this.txt_paswd.TabIndex = 0;
+            this.txt_paswd.DoubleClick += new System.EventHandler(this.txt_paswd_DoubleClick);
+            // 
+            // txt_DataInOut
+            // 
+            this.txt_DataInOut.AcceptsTab = true;
+            this.txt_DataInOut.Anchor = ((System.Windows.Forms.AnchorStyles)((((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Bottom) 
+            | System.Windows.Forms.AnchorStyles.Left) 
+            | System.Windows.Forms.AnchorStyles.Right)));
+            this.txt_DataInOut.BackColor = System.Drawing.SystemColors.ActiveCaptionText;
+            this.txt_DataInOut.BorderStyle = System.Windows.Forms.BorderStyle.None;
+            this.txt_DataInOut.Font = new System.Drawing.Font("Consolas", 14.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.txt_DataInOut.ForeColor = System.Drawing.Color.DarkGreen;
+            this.txt_DataInOut.Location = new System.Drawing.Point(1, 31);
+            this.txt_DataInOut.Name = "txt_DataInOut";
+            this.txt_DataInOut.Size = new System.Drawing.Size(798, 717);
+            this.txt_DataInOut.TabIndex = 2;
+            this.txt_DataInOut.Text = "";
+            this.txt_DataInOut.DoubleClick += new System.EventHandler(this.txt_DataInOut_DoubleClick);
+            // 
+            // timer1
+            // 
+            this.timer1.Interval = 10000;
+            this.timer1.Tick += new System.EventHandler(this.timer1_Tick);
+            // 
+            // timer2
+            // 
+            this.timer2.Interval = 35000;
+            this.timer2.Tick += new System.EventHandler(this.timer2_Tick);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.AutoSizeMode = System.Windows.Forms.AutoSizeMode.GrowAndShrink;
+            this.BackColor = System.Drawing.SystemColors.ActiveCaptionText;
+            this.ClientSize = new System.Drawing.Size(800, 749);
+            this.Controls.Add(this.txt_DataInOut);
+            this.Controls.Add(this.txt_paswd);
+            this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.FixedToolWindow;
+            this.HelpButton = true;
+            this.MaximizeBox = false;
+            this.Name = "Form1";
+            this.ShowInTaskbar = false;
+            this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
+            this.Text = "LOCK";
+            this.FormClosing += new System.Windows.Forms.FormClosingEventHandler(this.Form1_FormClosing);
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.TextBox txt_paswd;
+        private System.Windows.Forms.RichTextBox txt_DataInOut;
+        private System.Windows.Forms.Timer timer1;
+        private System.Windows.Forms.Timer timer2;
+    }
+}
+
---
+++ lock/Form1.cs
@@ -0,0 +1,192 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.IO;
+using System.Linq;
+using System.Runtime.InteropServices;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace @lock
+{
+    public partial class Form1 : Form
+    {
+        private int trial = 0;
+        private string paswd = "";
+        private string idea;
+        private string data;
+        private FileStream filestream;
+        private StreamReader strReader;
+        private StreamWriter strWriter;
+        /*
+        [DllImport("C:\\Users\\hp\\Desktop\\New folder\\code\\C++\\LOCK dll\\TryDll\\bin\\Debug\\TryDll.dll")]
+        static extern bool  save(string filename, string data);
+        [DllImport("C:\\Users\\hp\\Desktop\\New folder\\code\\C++\\LOCK dll\\TryDll\\bin\\Debug\\TryDll.dll")]
+        static extern string  get(string filename);
+
+
+        bool n = save("gg","g");
+        */
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void timer1_Tick(object sender, EventArgs e)
+        {
+            paswd = txt_DataInOut.Text;
+            txt_DataInOut.Text = "";
+            timer1.Stop();
+         }
+        private string OpenFile(string pswd)
+        {
+            string encData;
+            try
+            {
+                filestream = new FileStream(@"data.lock", FileMode.Open, FileAccess.Read);
+                strReader = new StreamReader(filestream);   
+                encData = strReader.ReadToEnd();
+                strReader.Close();
+                //filestream.Close();
+            }
+            catch(Exception ex)
+            {
+                MessageBox.Show(ex.Message,"open File");
+                //MessageBox.Show(ex.ToString());
+                //MessageBox.Show(ex.GetType().ToString());
+                if("System.IO.FileNotFoundException".Equals(ex.GetType().ToString()))
+                    filestream = File.Create("data.lock",1024);
+                encData = "";
+                filestream.Close();
+            }
+            data = unscramble(encData,toIntArray(pswd));
+            return data;
+        }
+        private void txt_paswd_DoubleClick(object sender, EventArgs e)
+        {
+            timer1.Start();
+            string pswd = txt_paswd.Text;
+            if (pswd.Equals(paswd))
+            {
+                /// TODO Get file
+                txt_DataInOut.Text = OpenFile(pswd);
+            }
+            else
+            {
+                if (trial < 3 )
+                {
+                    Console.Beep();
+                    MessageBox.Show("Wrong !!! Try Again");
+                    trial++;
+                }
+                else
+                {
+                    Application.Exit();
+                }
+
+            }
+        }
+
+        private void timer2_Tick(object sender, EventArgs e)
+        {
+            SaveExit(paswd);
+        }
+        private void SaveExit(string pwd)
+        {
+            try
+            {
+                if(filestream!= null)
+                    filestream.Close();
+                File.Delete("data.lock");
+                filestream = File.Create("data.lock", 1024);
+                strWriter = new StreamWriter(filestream);
+
+                try
+                {
+                    if (txt_DataInOut.Text != data && txt_DataInOut.Text == "")
+                        strWriter.Write(scramble(data, toIntArray(pwd)));
+                    else
+                        strWriter.Write(scramble(txt_DataInOut.Text, toIntArray(pwd)));
+                } catch(Exception ex)
+                {
+                    MessageBox.Show(ex.Message);
+                }
+            }
+            catch(Exception ex)
+            {
+                MessageBox.Show(ex.Message,"save file");
+            }
+            Application.Exit();
+        }
+
+        private void txt_DataInOut_DoubleClick(object sender, EventArgs e)
+        {
+            // Save data if any change
+            int[] cy = toIntArray("Plmzaq&oknxsw2");
+            string s = scramble(txt_DataInOut.Text, cy);
+            string uns = unscramble(s , cy);
+            txt_DataInOut.Text = Convert.ToString(s );
+            txt_DataInOut.Text = txt_DataInOut.Text + "\n"+uns;
+
+        }
+
+        private string scramble(string s,int[] cypher)
+        {
+            char [] s2 = s.ToCharArray();
+            string scrambled = "";
+            
+            for (int i = 0,j=0; i < s2.Length;j++, i++) 
+            {
+                s2[i] = Convert.ToChar( Convert.ToInt32( s2[i] ) + cypher[j]);
+                if (cypher.Length-1 == j)
+                {
+                    j = -1;
+                }
+            
+            }
+            for (int i = 0; i < s2.Length; i++)
+            {
+                scrambled += s2[i];
+            }
+            return scrambled;
+        }
+        private string unscramble(string s, int[] cypher)
+        {
+            char[] s2 = s.ToCharArray();
+            string unscrambled = "";
+            
+            for (int i = 0, j = 0; i < s2.Length; j++, i++)
+            {
+                s2[i] = Convert.ToChar(Convert.ToInt32(s2[i]) - cypher[j]);
+                if (cypher.Length - 1 == j)
+                {
+                    j = -1;
+                }
+
+            }
+            for (int i = 0; i < s2.Length; i++)
+            {
+                unscrambled += s2[i];
+            }
+            return unscrambled;
+        }
+        private int[] toIntArray(string s)
+        {
+            int[] array = new int[s.Length];
+            for (int i = 0; i < s.Length; i++)
+            {
+                array[i] = Convert.ToInt32(s[i]);
+            }
+            return array;
+        }
+
+        private void Form1_FormClosing(object sender, FormClosingEventArgs e)
+        {
+            SaveExit(paswd);
+        }
+        
+    }
+}
